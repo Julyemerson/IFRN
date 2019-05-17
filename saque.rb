be9cd465caf92f5260ab2e8cb4299be4a23b5664
Julyemerson <jb.leonizio@gmail.com>
2019-05-17 15:06:53 -0300

    Logica do caixa eletronico pronto IFRN

---
+++ saque.rb
@@ -0,0 +1,37 @@
+puts "Saque"
+puts " Digite um valor para Sacar no caixa eletronico do IFRN "
+
+valor = gets.chomp.to_i 
+
+total = valor 
+ced = 100
+total_ced = 0 
+
+loop do
+    if total >= ced
+        total -= ced
+        total_ced += 1
+    else
+        if total_ced > 0
+            puts " O total de #{total_ced}, cedulas de R$#{ced}"
+        end
+        if ced == 100
+            ced = 50
+        elsif ced == 50
+            ced = 20
+        elsif ced == 20
+            ced = 10
+        elsif ced == 10
+            ced = 1
+        end
+        total_ced = 0 
+        break if total == 0 
+    end
+    
+end
+  
+
+
+
+
+    
