9ec8b9d7cf107ad591cae3a1ccfc5c8a9656613e
Julyemerson <jb.leonizio@gmail.com>
2019-05-20 12:50:57 -0300

    Atividade Finalizada.

--- saque.rb
+++ saque.rb
@@ -11,31 +11,35 @@ loop do
             valor = gets.chomp.to_i 
             total = valor 
             ced = 100
-            total_ced = 0 
-
-        loop do
-            if total >= ced
-                total -= ced
-                total_ced += 1
-            else
-                if total_ced > 0
-                    puts " O total de #{total_ced}, cedulas de R$ #{ced}"
+            total_ced = 0
+        
+        if (valor % 2 == 0 || valor % 5 == 0 || valor % 10 == 0 || valor % 20 == 0 || valor % 50 == 0 || valor % 100 == 0) 
+            loop do
+                if total >= ced
+                    total -= ced
+                    total_ced += 1
+                else
+                    if total_ced > 0
+                        puts " O total de #{total_ced}, cedulas de R$ #{ced}"
+                    end
+                    if ced == 100
+                        ced = 50
+                    elsif ced == 50
+                        ced = 20
+                    elsif ced == 20
+                        ced = 10
+                    elsif ced == 10
+                        ced = 5
+                    elsif ced == 5
+                        ced = 2
+                    end
+                    total_ced = 0 
+                    break if total == 0 
                 end
-                if ced == 100
-                    ced = 50
-                elsif ced == 50
-                    ced = 20
-                elsif ced == 20
-                    ced = 10
-                elsif ced == 10
-                    ced = 5
-                elsif ced == 5
-                    ced = 2
-                end
-                total_ced = 0 
-                break if total == 0 
-            end
-        end 
+            end 
+        else
+            puts " O valor solicitado deve ser múltiplo de R$ 2, R$ 5, R$ 10, R$ 20, R$ 50 ou R$ 100 reais! "
+        end    
     end
     break if opcao == 0     
 end     
