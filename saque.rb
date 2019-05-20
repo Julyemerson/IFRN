1b99b1b39bc67ecc0ed088d95f398bdb7eed730e
Julyemerson <jb.leonizio@gmail.com>
2019-05-20 11:17:23 -0300

    Adicionando menu solicitado na ativadade

--- saque.rb
+++ saque.rb
@@ -1,35 +1,42 @@
-puts "Saque"
-puts " Digite um valor para Sacar no caixa eletronico do IFRN "
-
-valor = gets.chomp.to_i 
-
-total = valor 
-ced = 100
-total_ced = 0 
-
 loop do
-    if total >= ced
-        total -= ced
-        total_ced += 1
-    else
-        if total_ced > 0
-            puts " O total de #{total_ced}, cedulas de R$#{ced}"
-        end
-        if ced == 100
-            ced = 50
-        elsif ced == 50
-            ced = 20
-        elsif ced == 20
-            ced = 10
-        elsif ced == 10
-            ced = 1
-        end
-        total_ced = 0 
-        break if total == 0 
-    end
+
+    puts " Escolha a operação que deseja realizar? "
+    puts " 1 - Saque "
+    puts " 0 - Sair "
     
-end
-  
+    opcao = gets.chomp.to_i 
+
+        if opcao == 1     
+            puts " Digite um valor para Sacar no caixa eletronico do IFRN "
+            valor = gets.chomp.to_i 
+            total = valor 
+            ced = 100
+            total_ced = 0 
+
+        loop do
+            if total >= ced
+                total -= ced
+                total_ced += 1
+            else
+                if total_ced > 0
+                    puts " O total de #{total_ced}, cedulas de R$#{ced}"
+                end
+                if ced == 100
+                    ced = 50
+                elsif ced == 50
+                    ced = 20
+                elsif ced == 20
+                    ced = 10
+                elsif ced == 10
+                    ced = 1
+                end
+                total_ced = 0 
+                break if total == 0 
+            end
+        end 
+    end
+    break if opcao == 0     
+end   
 
 
 
